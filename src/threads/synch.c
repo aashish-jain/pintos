2200d6f86b2ccc89ff4dda73b6c58191dd9c12a3
Aashish Jain <aashish_jain@live.com>
2018-10-18 04:33:34 -0400

    priority-condvar now works

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -32,6 +32,8 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
+
+bool cond_order_func(struct list_elem *, struct list_elem *, void *);
 /* Initializes semaphore SEMA to VALUE.  A semaphore is a
    nonnegative integer along with two atomic operators for
    manipulating it:
@@ -273,6 +275,10 @@ struct semaphore_elem
   {
     struct list_elem elem;              /* List element. */
     struct semaphore semaphore;         /* This semaphore. */
+    //Added begins
+    //For thread's priority (thread * is better - can change)
+    struct thread *t;
+    //Added ends
   };
 
 /* Initializes condition variable COND.  A condition variable
@@ -317,7 +323,13 @@ cond_wait (struct condition *cond, struct lock *lock)
   ASSERT (lock_held_by_current_thread (lock));
   
   sema_init (&waiter.semaphore, 0);
-  list_push_back (&cond->waiters, &waiter.elem);
+  //Acutal code
+  // list_push_back (&cond->waiters, &waiter.elem);
+
+  //Added begins
+  waiter.t = thread_current();
+  list_insert_ordered(&cond->waiters, &waiter.elem, cond_order_func, NULL);
+  //Added ends
   lock_release (lock);
   sema_down (&waiter.semaphore);
   lock_acquire (lock);
@@ -401,4 +413,12 @@ sema_down_ordered (struct semaphore *sema, list_less_func *order_by, void *aux)
     }
   sema->value--;
   intr_set_level (old_level);
+}
+
+//Added
+bool
+cond_order_func(struct list_elem *a, struct list_elem *b, void *aux UNUSED){
+  struct semaphore_elem *x = list_entry(a, struct semaphore_elem, elem);
+  struct semaphore_elem *y = list_entry(b, struct semaphore_elem, elem);
+  return x->t->priority > y->t->priority; 
 }
